b2b9572b7f61ac34fc9e859bc722d9676bb8fef6
master <sist.varm@gmail.com>
2024-09-30 13:18:13 +0900

    Project Initialize

---
+++ src/main/java/com/web/zoe/ZoeApplication.java
@@ -0,0 +1,13 @@
+package com.web.zoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoeApplication.class, args);
+	}
+
+}
